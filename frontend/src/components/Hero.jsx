37376816747918a2b49a9710113ca9c7252251b5
Mahmoud Elkazzaz <mhkazazz@hotmail.com>
2024-03-20 22:18:44 +1100

    Testing API Fetching

--- frontend/src/components/Hero.jsx
+++ frontend/src/components/Hero.jsx
@@ -1,13 +1,34 @@
 // import { Container, Card, Button } from "react-bootstrap";
 // import { LinkContainer } from "react-router-bootstrap";
 // import { Link } from "react-router-dom";
+
+import { useState, useEffect } from "react";
 import "../styles/hero.scss";
 import "../screens/LoginScreen";
 
 const Hero = () => {
+  const [englishName, setEnglishName] = useState("");
+  const [img, setImg] = useState("");
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch(
+          "https://yoga-api-nzy4.onrender.com/v1/poses"
+        );
+        const jsonData = await response.json();
+        setEnglishName(jsonData[0].english_name);
+        setImg(jsonData[0].url_svg);
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
+    };
+
+    fetchData();
+  }, []);
   return (
     <div className="hero">
-      
+      <div className="name">{englishName}</div>
+      <img src={img} alt={englishName} />
     </div>
   );
 };
