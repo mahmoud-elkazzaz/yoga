098c29643138df6e7756b122a156741d493b03a9
Mahmoud Elkazzaz <mhkazazz@hotmail.com>
2024-03-20 18:54:11 +1100

    Sign-in Page

---
+++ frontend/src/components/Hero.jsx
@@ -0,0 +1,15 @@
+// import { Container, Card, Button } from "react-bootstrap";
+// import { LinkContainer } from "react-router-bootstrap";
+// import { Link } from "react-router-dom";
+import "../styles/hero.scss";
+import "../screens/LoginScreen";
+
+const Hero = () => {
+  return (
+    <div className="hero">
+      
+    </div>
+  );
+};
+
+export default Hero;
---
+++ frontend/src/screens/LoginScreen.jsx
@@ -0,0 +1,82 @@
+import { useState, useEffect } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { Form, Button, Row, Col } from "react-bootstrap";
+import { useDispatch, useSelector } from "react-redux";
+import FormContainer from "../components/FormContainer";
+import { useLoginMutation } from "../slices/usersApiSlice";
+import { setCredentials } from "../slices/authSlice";
+import { toast } from "react-toastify";
+import Loader from "../components/Loader";
+import "../styles/LoginScreen.scss";
+
+const LoginScreen = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+
+  const [login, { isLoading }] = useLoginMutation();
+
+  const { userInfo } = useSelector((state) => state.auth);
+
+  useEffect(() => {
+    if (userInfo) {
+      navigate("/");
+    }
+  }, [navigate, userInfo]);
+
+  const submitHandler = async (e) => {
+    e.preventDefault();
+    try {
+      const res = await login({
+        email,
+        password,
+      }).unwrap();
+      dispatch(setCredentials({ ...res }));
+      navigate("/");
+    } catch (err) {
+      toast.error(err?.data?.message || err.error);
+    }
+  };
+  return (
+    <div className="login">
+      <div className="login__img"></div>
+      <div className="login__container">
+        <h1 className="login__title">Sign In</h1>
+        <form className="login__form" onSubmit={submitHandler}>
+          <div className="login__form__field">
+            <label htmlFor="email">Email:</label>
+            <input
+              type="email"
+              id="email"
+              name="email"
+              placeholder="Enter Email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            ></input>
+          </div>
+          <div className="login__form__field">
+            <label htmlFor="password">Password:</label>
+            <input
+              type="password"
+              id="password"
+              name="password"
+              placeholder="Enter Password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            ></input>
+          </div>
+          {isLoading && <Loader />}
+          <button className="login__form__btn" type="submit">
+            Sign In
+          </button>
+          <div className="login__new">
+            New Customer? <Link to="/register">Register</Link>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default LoginScreen;
